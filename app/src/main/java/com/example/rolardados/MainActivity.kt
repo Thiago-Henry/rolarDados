4a7fff1a2e5667c4e69682c8fda09eba0ba00e92
Thiago-Henry <thiago757475@gmail.com>
2023-10-19 08:27:52 -0300

    teste2

--- app/src/main/java/com/example/rolardados/MainActivity.kt
+++ app/src/main/java/com/example/rolardados/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.rolardados
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rolardados.ui.theme.RolarDadosTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +35,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RolarDadosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            appRolarDado()
             }
         }
     }
 }
-
+@Preview(showSystemUi = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun appRolarDado(){
+    var resultado by remember { mutableStateOf(1) }
+    var idImagem = when(resultado){
+        1-> R.drawable.dice_1
+        2-> R.drawable.dice_2
+        3-> R.drawable.dice_3
+        4-> R.drawable.dice_4
+        5-> R.drawable.dice_5
+        else -> R.drawable.dice_6
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RolarDadosTheme {
-        Greeting("Android")
     }
-}
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+        ){
+        androidx.compose.foundation.Image(
+            painter = painterResource(id = idImagem) ,
+            contentDescription =null,
+        )
+        Button(
+            colors = ButtonDefaults.buttonColors(Color.Gray),
+            onClick = {
+                resultado = (1..6).random()
+            }
+        ) {
+         Text(
+             text ="Trocar Dados",
+             fontSize = 20.sp,
+         )
+        }
+        }
+    }
